175fdd2904ff124bda352349106c98b93e203548
Katerina Lina Papaloukas <katpapaloukas@gmail.com>
2020-12-29 12:32:09 -0500

    trying to solve ssl problem

--- server/db/db.js
+++ server/db/db.js
@@ -2,19 +2,29 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
 const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
-const client = new Sequelize(dbUrl, { logging: false, operatorsAliases: false,
-dialect: 'postgres',
-protocol: 'postgres',
-ssl: true,
-dialectOptions: {
-  ssl: true,
-},
-});
+// const client = new Sequelize(dbUrl, { logging: false, operatorsAliases: false,
+// dialect: 'postgres',
+// protocol: 'postgres',
+// ssl: true,
+// dialectOptions: {
+//   ssl: true,
+// },
+// });
 
-module.exports = client
+//module.exports = client
 
 // This is a global Mocha hook used for resource cleanup.
 // Otherwise, Mocha v4+ does not exit after tests.
-if (process.env.NODE_ENV === 'test') {
-  after('close database connection', () => client.close())
-}
+// if (process.env.NODE_ENV === 'test') {
+//   after('close database connection', () => client.close())
+// }
+
+
+const client = new Sequelize(dbUrl, {  connectionString: process.env.DATABASE_URL,
+  logging: false,
+  operatorsAliases: false,
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {    ssl: {      rejectUnauthorized: false,    },  },})
+
+client.query(  'SELECT table_schema,table_name FROM information_schema.tables;',  (err, res) => {    if (err) throw err;    for (let row of res.rows) {      console.log(JSON.stringify(row))    }    client.end()  })
